13a6609644e0b1e7f8d47d7bbf71374286f95f46
kavyamolps <115360019+kavyamolps@users.noreply.github.com>
2023-01-20 20:15:30 +0530

    Add files via upload

---
+++ 10_naturalnumbers_sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int sum=0;
+    for(i=1;i<=10;i++)
+    {
+        sum=sum+i;
+    }
+    printf("SUM OF FIRST 10 NATURAL NUMBERS:%d",sum);
+    return 0;
+}
